38338d13fe626b33ef71cc4dfd4154980e6e2289
LuccasDeLuz <116004060+LuccasDeLuz@users.noreply.github.com>
2022-11-23 11:54:48 -0300

    Add files via upload

---
+++ luccas/JavaScript/main.js
@@ -0,0 +1,39 @@
+console.log("Luccas");
+console.log(123456);
+
+var nome= "Luccas"
+let idade = 17;
+const pi = 3.14;
+
+console.log(nome);
+console.log(idade);
+console.log(pi);
+
+idade = 1;
+console.log(idade);
+
+/******************************/
+let x = 3;
+if (x > 0) {     //estrutura condicional
+    console.log("Oi");
+}
+
+let y = 2;
+while (y < 5) {//laço de repetição enquanto
+    y += 1;
+    console.log(y);
+}
+
+ let z = 1;
+ for (var i = 0; i<10; i++){//laço de repetição para
+    console.log(z);
+    z += 45;
+}
+
+/******************************/
+let xy = [3, 5, 7, 7, 9]
+console.log(xy);
+console.log(xy[2]);
+
+/******************************/
+
